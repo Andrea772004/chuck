ed795068de7885a46930db5d56a0a981b03e5b2d
Manuel Manna <>
2023-02-18 11:58:56 +0100

    inizio lavoro

---
+++ src/componets/Titolo.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import '../styles/Titolo.css'
+
+function Titolo() {
+
+  return (
+    <div className="Titolo">
+      
+    </div>
+  )
+}
+
+export default Titolo
