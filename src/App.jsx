3241ed4eb1d158b9bf5c1bc52a246ae2f0ca3f13
Andrea772004 <115874097+Andrea772004@users.noreply.github.com>
2023-03-10 18:18:56 +0100

    Creazione della funzione callback e bottone disable e hover

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,24 @@ import Button from './componets/Button'
 
 function App() {
   const [count, setCount] = useState(0)
-
   const [joke, setjoke] = useState(0)
+
+  let loadJokeCallback = function (){
+    console.log ("ciao")
+    setJoke("testo joke")
+  }
+  
+  let copyTextCallback = function (){
+    console.log ("bye")
+  }
+
   return (
     <div className="App">
       <h1>Webapp API Chuck Norris</h1>
       <p>Una pagina che sfrutta Api di Chuck Norris per generare un testo</p>
 
-      <Button text='Carica il joke'/>
-      <Button text='Copia'/>
+      <Button text='Carica il joke' callback={loadJokeCallback}/>
+      <Button text='Copia' variant={ joke == "" ? "disabled" : undefined}callback={copyTextCallback}/>
     </div>
   )
 }
--- src/componets/Button.jsx
+++ src/componets/Button.jsx
@@ -2,16 +2,34 @@ import { useState } from 'react'
 import '../styles/Button.css'
 import  './Titolo'
 
-function Button({text}) {
+function Button({text,variant,callback}) {
   const [count, setCount] = useState(0)
+  let clickHandler = function(e){
+    console.log(e)
+    console.log(typeof undefined)
+  }
+
+  classRenderer = function (){
+    let classes = [
+      'Button', variant
+    ]
+    return classes.join(" ")
+  }
 
   return (
+    <div onClick={clickHandler} className={classRenderer()}>
+
+   return (
     <div className="Button">
 
      {text}
-
+     </div>
+  )
     </div>
+  
   )
 }
 
-export default Button 
+export default Button
+
+
